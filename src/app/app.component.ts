78be0759d42d8748ac2452537fef15019a361ce9
mdthameem2502 <mdthameem2502@gmail.com>
2022-08-15 20:39:43 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,24 +5,36 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   registerForm: FormGroup;
   submitted = false;
-  list: any = {
-    FirstName: 'sss',
-    LastName: 'dshjk',
-    Email: 'abcd@gmail.com',
-    Phone: 987456321,
-    Company: 'Aegis',
-    DOB: '25-01-1996',
-  };
+  list: any = [{}];
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
+      company: ['', false],
+      Phone: ['', false],
+      dob: ['', false],
       email: ['', [Validators.required, Validators.email]],
     });
   }
 
+  onRegister() {
+    let pushObj = {
+      firstName: this.registerForm.get('firstName').value,
+      lastName: this.registerForm.get('lastName').value,
+      email: this.registerForm.get('email').value,
+      phone: this.registerForm.get('Phone').value,
+      company: this.registerForm.get('company').value,
+      dob: this.registerForm.get('dob').value,
+    };
+    localStorage.setItem('pushObj', JSON.stringify(pushObj));
+    let fNameLoc = JSON.parse(localStorage.getItem('pushObj'));
+    this.list.push(pushObj);
+    alert(
+      'SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4)
+    );
+  }
   get f() {
     return this.registerForm.controls;
   }
@@ -33,10 +45,6 @@ export class AppComponent implements OnInit {
     if (this.registerForm.invalid) {
       return;
     }
-
-    alert(
-      'SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4)
-    );
   }
 
   onReset() {
